ce11dc63799e352ff17feeee187356e26bb7db7c
Tarciana Oliveira <anarumiroan@gmail.com>
2023-01-04 21:38:08 -0300

    primeiro commit

---
+++ coresemcss.py
@@ -0,0 +1,21 @@
+
+hexadecimal 0123456ABCDEF
+rgb= red green blue
+#_ _ _ _ _ _
+dois primeiros para marcar o vermelho
+dois próximos marcar o verde
+dois últomos marcar o azul
+0 = ausência de cor
+F= máximo de cor
+#FF0000 -> vermelho
+#00FF00 -> VERDE
+#0000FF ->AZUL
+
+RGB = 0,1,2,3,4,5 até 255
+0= ausência
+255=máximo
+rgb(255,255,255)
+
+#FF0000 = (255,0,0) -> vermelho
+#00FF00 = (0,255,0) -> verde
+#0000FF = (0,0,255) -> AZUL
